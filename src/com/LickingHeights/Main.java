d839aeed2745d957d0f9fe871fe59163849febf1
Shane <shane.newland.2023@student.lhschools.org>
2020-02-25 13:05:13 -0500

    IntelliJ Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is");
+        System.out.println(" Shane");
+        System.out.println("This summer I broke my foot.");
+        System.out.println("Started my first year in football");
+        System.out.println("Started my first year of highschool");
+
+        //Print three more lines about what you did last summer
+
+    }
+}
